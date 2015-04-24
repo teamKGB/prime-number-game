fbd0b0e504b38c1111a23de5c53f0dc4de6f641e
Jimmy <Jimmy090804@gmail.com>
2015-04-23 22:12:59 -0500

    Added title bar and tool bar to gameWindow.swift

--- Prime-Game/Prime-Game/GameWindow.swift
+++ Prime-Game/Prime-Game/GameWindow.swift
@@ -10,18 +10,32 @@ import UIKit
 
 class GameWindow: UIViewController {
     
+
+    @IBOutlet weak var titleBar: UINavigationBar!
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    
     var timeLeft: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        changeStyle() 
         addBlock()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func changeStyle() {
+        //change the title bar color to white
+        titleBar.barTintColor = UIColor.whiteColor()
+        
+        //change the tool bar color to white
+        toolBar.barTintColor = UIColor.whiteColor()
     }
     
     func addBlock() {
