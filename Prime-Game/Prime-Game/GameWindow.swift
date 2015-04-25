1b8a9d5b3ff948857efbe1bddbec088b9fdea7c1
Jimmy <Jimmy090804@gmail.com>
2015-04-24 19:18:28 -0500

    Done with the grid thing

--- Prime-Game/Prime-Game/GameWindow.swift
+++ Prime-Game/Prime-Game/GameWindow.swift
@@ -19,8 +19,9 @@ class GameWindow: UIViewController {
     @IBOutlet weak var gameWindowView: UIView!
     
     
-    var timeLeft: Int!
-    var gameLevel: Int!
+    var timeLeft: Int = 0
+    var gameLevel: Int = 0
+    var blockNumbers: Int = 0;
     
     private var xOffset: CGFloat = 0.0
     private var yOffset: CGFloat = 0.0
@@ -32,12 +33,78 @@ class GameWindow: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        changeStyle() 
-        //makeBlock(5)
-        var newView = block(frame: CGRectMake(5, 7, 40, 40))
-        gameWindowView.addSubview(newView)
+        changeStyle()
+        
+        gameStart()
+        
+    }
+    
+    func gameStart() {
+        gameLevel += 1 //increment level by 1 for each level
+        levelLable.text = String(gameLevel)
+        
+        //check how many block to produce in one level
+        if(gameLevel < 3){
+            
+            timeLeft = 40
+            while (blockNumbers < 5){
+                blockNumbers = Int(arc4random_uniform(30)) + 1
+            }
+        }
+        else if (gameLevel < 8) {
+            while (blockNumbers < 10){
+                blockNumbers = Int(arc4random_uniform(20)) + 1
+            }
+            timeLeft = 30
+        }
+        else {
+            while (blockNumbers < 15){
+                blockNumbers = Int(arc4random_uniform(30)) + 1
+            }
+            timeLeft = 20
+        }
+        
+        //make blocks
+        
+        for(var i = 0; i < blockNumbers; i++){
+            xOffset = randomBetweenNumbers(1, secondNum: gameWindowView.frame.size.width)
+            yOffset = randomBetweenNumbers(1, secondNum: gameWindowView.frame.size.height)
+            var newBlockView = block(frame: CGRectMake(xOffset, yOffset, 40, 40))
+            
+            println("this is new x:  \(newBlockView.center.x)")
+            println("this is new y: \(newBlockView.center.y)")
+            
+            // set grid size
+            var size = 40.0
+            //n.frame = CGRectMake(pointX, pointY, 60, 60)
+            
+            // snap to the grid
+            newBlockView.center = CGPointMake(CGFloat(size) * CGFloat(floor(newBlockView.center.x/CGFloat(size))), CGFloat(size) * CGFloat(floor(newBlockView.center.y/CGFloat(size))))
+            
+            newBlockView.center.x += 27.5
+            newBlockView.center.y += 39.5
+            
+            if (newBlockView.center.x - 27.5 < 0) {
+                newBlockView.center.x = 27.5
+            }
+            
+            if (newBlockView.center.x + 27.5 > gameWindowView.frame.size.width) {
+                newBlockView.center.x = gameWindowView.frame.size.width - 27.5
+            }
+            
+            if (newBlockView.center.y - 20 < 19.5) {
+                newBlockView.center.y = 19.5 + 20
+            }
+            
+            if (newBlockView.center.y + 20 > gameWindowView.frame.size.height - 39.5) {
+                newBlockView.center.y = gameWindowView.frame.size.height - 39.5
+            }
+            
+            gameWindowView.addSubview(newBlockView)
+        }
+        
+        
         
     }
     
@@ -46,8 +113,9 @@ class GameWindow: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
+    func randomBetweenNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat{
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
     
     func changeStyle() {
     
